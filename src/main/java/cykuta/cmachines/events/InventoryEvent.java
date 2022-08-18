e4c33f81529340a09e601ae0a3e064c2c2731e34
Mau <mautdhd@gmail.com>
2022-08-18 12:44:40 -0400

    feat (InventoryOpenEvent): added some utils variables

--- src/main/java/cykuta/cmachines/events/InventoryEvent.java
+++ src/main/java/cykuta/cmachines/events/InventoryEvent.java
@@ -1,17 +1,21 @@
 package cykuta.cmachines.events;
 
+import cykuta.cmachines.blocks.Machine;
 import cykuta.cmachines.blocks.MachineHolder;
 import cykuta.cmachines.fileManagment.MachineData;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.event.inventory.InventoryOpenEvent;
+import org.bukkit.inventory.Inventory;
 
 public class InventoryEvent implements Listener {
     private final MachineData data = new MachineData();
 
     @EventHandler
-    private void onPlayerCloseInventory(InventoryCloseEvent e){
+    private void onPlayerCloseInventory(InventoryOpenEvent e){
         // Check if is machine via Holder
         if (!(e.getInventory().getHolder() instanceof MachineHolder)) return;
+        Inventory inventory = e.getInventory();
+        Machine machine = ((MachineHolder) e.getInventory().getHolder()).getMachine();
     }
 }
