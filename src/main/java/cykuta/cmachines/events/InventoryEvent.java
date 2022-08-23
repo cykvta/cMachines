c6a1627f0954182bf5bf6f7c8a71204d88d24afb
Mau <mautdhd@gmail.com>
2022-08-23 19:01:22 -0400

    fix (Inventory Event): Removed old checks and replace with a better solution

--- src/main/java/cykuta/cmachines/events/InventoryEvent.java
+++ src/main/java/cykuta/cmachines/events/InventoryEvent.java
@@ -2,20 +2,35 @@ package cykuta.cmachines.events;
 
 import cykuta.cmachines.blocks.Machine;
 import cykuta.cmachines.blocks.MachineHolder;
-import cykuta.cmachines.fileManagment.MachineData;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryOpenEvent;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryHolder;
 
 public class InventoryEvent implements Listener {
-    private final MachineData data = new MachineData();
 
     @EventHandler
-    private void onPlayerCloseInventory(InventoryOpenEvent e){
-        // Check if is machine via Holder
+    public void inventoryInteract(InventoryClickEvent e){
+        if (e.getClickedInventory() == null) return;
         if (!(e.getInventory().getHolder() instanceof MachineHolder)) return;
-        Inventory inventory = e.getInventory();
-        Machine machine = ((MachineHolder) e.getInventory().getHolder()).getMachine();
+        if (e.isShiftClick()) {
+            e.setCancelled(true);
+            return;
+        }
+        if (!(e.getClickedInventory().getHolder() instanceof MachineHolder)) return;
+
+        switch (e.getSlot()){
+            case 20:
+                break;
+            case 24:
+                break;
+            default:
+                e.setCancelled(true);
+                break;
+        }
+
+        e.getWhoClicked().sendMessage(e.getSlot() + "");
     }
 }
